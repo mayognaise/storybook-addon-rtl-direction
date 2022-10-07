00a27647c67d3b9b8ca06c814376664f03050ee5
Mayo Tobita <tobimayo@gmail.com>
2022-10-07 12:52:30 -1000

    Fix dir attribute tag to be html

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 An addon to toggle between ltr and rtl mode.
 
-- `direction: ltr` or `direction: rtl` style will be added to `body` tag with `←` icon in tool section.
-- Add `dir` attribute to body tag for reference as well.
+- `dir: ltr` or `dir: rtl` style will be added to `html` tag with `LTR/RTL` icon in tool section.
 
 ![Example](https://user-images.githubusercontent.com/588874/192465178-2d8483e7-b950-48da-a063-c0545d1b0b29.gif)
 
--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -2,13 +2,26 @@ import type { DecoratorFunction } from "@storybook/addons";
 import { useEffect, useGlobals } from "@storybook/addons";
 
 export const withGlobals: DecoratorFunction = (StoryFn) => {
-  const [{ rtlDirection }] = useGlobals();
+  const [{ rtlDirection }, updateGlobals] = useGlobals();
 
   useEffect(() => {
     const direction = rtlDirection ? "rtl" : "ltr";
-    document.body.style.setProperty("direction", direction);
-    document.body.setAttribute("dir", direction);
+    document.documentElement.setAttribute("dir", direction);
   }, [rtlDirection]);
+
+  useEffect(() => {
+    // Check url
+    const urlParams = new URLSearchParams(window.location.search);
+    const globals = urlParams.get("globals");
+    if (!globals) return;
+    const [dir] = globals
+      .split(";")
+      .map((params) => {
+        return params.indexOf("rtlDirection:") === 0 && params.split(":")[1];
+      })
+      .filter(Boolean);
+    if (dir === "true") updateGlobals({ rtlDirection: true });
+  }, []);
 
   return StoryFn();
 };
